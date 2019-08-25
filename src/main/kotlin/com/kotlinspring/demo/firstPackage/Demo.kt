94f279fb1d6f8c7389c3ab5143ee2aeb1dff9444
Lovika Saxena <Lovika_Saxena@external.mckinsey.com>
2019-08-26 01:50:51 +0530

    [Lovika]: Added logs in Demo controller, corrected package name

--- src/main/kotlin/com/kotlinspring/demo/firstPackage/Demo.kt
+++ src/main/kotlin/com/kotlinspring/demo/firstPackage/Demo.kt
@@ -1,15 +1,24 @@
-package com.kotlinspring.starter.firstPackage
+package com.kotlinspring.demo.firstPackage
 
+import jdk.nashorn.internal.runtime.regexp.joni.Config.log
+import lombok.extern.slf4j.Slf4j
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
+@Slf4j
 class Demo{
 
     @GetMapping("/")
-    fun welcomePage(): String = "Welcome to Kotlin-Spring demo project"
+    fun welcomePage(): String {
+        log.println("Default end point")
+        return "Welcome to Kotlin-Spring demo project"
+    }
 
     @GetMapping("/welcome")
-    fun dummyApi(): String = "Kotlin-Spring: A match made in heaven"
+    fun dummyApi(): String {
+        log.println("Welcome end point")
+        return "Kotlin-Spring: A match made in heaven"
+    }
 }
