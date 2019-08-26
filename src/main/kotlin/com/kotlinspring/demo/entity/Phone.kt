c9193f795a8fb9d1cde6671ad0a04b3b031924c4
Lovika Saxena <Lovika_Saxena@external.mckinsey.com>
2019-08-26 14:57:14 +0530

    [Lovika]: Added method to PhoneRepository to fetch phones by brand

--- src/main/kotlin/com/kotlinspring/demo/entity/Phone.kt
+++ src/main/kotlin/com/kotlinspring/demo/entity/Phone.kt
@@ -1,2 +1,11 @@
 package com.kotlinspring.demo.entity
+import org.neo4j.ogm.annotation.NodeEntity
+import org.neo4j.ogm.annotation.Property
 
+@NodeEntity
+class Phone(
+            //@Id var id: Long,
+        @Property("brand") var brand: String,
+        @Property("model") var name: String,
+        @Property("cost") var cost: Float
+        )
--- src/main/kotlin/com/kotlinspring/demo/repository/PhoneRepository.kt
+++ src/main/kotlin/com/kotlinspring/demo/repository/PhoneRepository.kt
@@ -1,2 +1,13 @@
 package com.kotlinspring.demo.repository
 
+import com.kotlinspring.demo.entity.Phone
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PhoneRepository : PagingAndSortingRepository<Phone, Long> {
+
+    fun findByBrand(@Param("brand") brand: String): Phone?
+
+}
